ae17cf678b4187d53030fa449443e08b48364bcb
vineves-cloud <vinevesalvorada@gmail.com>
2026-04-07 19:37:35 +0000

    primeiros passos

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Olá, mundo! Meu primeiro projeto no GitHub 🚀")
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+response = requests.get(url)
+
+data = response.json()
+
+print(data["name"])
+print(data["height"])
